012a521661e08865e98721027b2937cd9fd01c80
yeOmaNnn <cool.rtj2016@yandex.ru>
2024-05-22 14:29:49 +0300

    update

---
+++ sso/cmd/sso/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"sso/Internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+	log.Info("status application", slog.Any("config", cfg))
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
